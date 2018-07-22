5e2e9c17429df7a4b9408d44e75903ed451abc32
马治理 <526877521@qq.com>
2018-07-22 10:22:56 +0800

    测试推送

--- assets/core/msg/Observer.js
+++ assets/core/msg/Observer.js
@@ -8,7 +8,6 @@ cc.Class({
             let msg = list[k];
             ObserverMgr.addEventListener(msg, this._onMsg, this);
         }
-        // ObserverMgr.addEventListener(GameMsgGlobal.Net.MsgErr, this._onErrorDeal, this);
     },
     onLoad() {
     },
